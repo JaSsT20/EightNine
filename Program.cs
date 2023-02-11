3600d0a34f6b8ea780e4504edb90bac495a9ad91
Levid <jassontejada@gmail.com>
2023-02-11 11:14:01 -0400

    Adding function ShowAlphabeticOrder()

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            //CAP 8 -> EX 3
             ShowActualDate();
+            //CAP 8 -> EX 5
+            ShowAlphabeticOrder();
         }
 
         public static void ShowActualDate()
@@ -15,7 +18,29 @@
             Console.WriteLine($"Year: {date.Year}");
             Console.WriteLine($"Month:{date.Month}");
             Console.WriteLine($"Day: {date.Day}");
+            Console.Write("Presione alguna tecla para continuar...");
+            Console.ReadLine();
+        }
 
+        public static void ShowAlphabeticOrder()
+        {
+            Console.Write("Ingrese cadena#1: ");
+            string firstString = Console.ReadLine();
+
+            Console.Write("Ingrese cadena#2: ");
+            string secondString = Console.ReadLine();
+
+            if (firstString.CompareTo(secondString) < 0)
+            {
+                Console.WriteLine($"1- {firstString}");
+                Console.WriteLine($"2- {secondString}");
+            }
+            else
+            {
+                Console.WriteLine($"1- {secondString}");
+                Console.WriteLine($"2- {firstString}");
+            }
+            Console.Write("Presione alguna tecla para continuar...");
             Console.ReadLine();
         }
     }
