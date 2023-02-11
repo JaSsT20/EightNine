57d0d5fd131a08b8fd533eb46b616ec1c311f879
Levid <jassontejada@gmail.com>
2023-02-11 11:02:08 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ActualDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            Console.WriteLine($"Hour: {date.ToString("hh:mm tt")}");
+            Console.WriteLine($"Year: {date.Year}");
+            Console.WriteLine($"Month:{date.Month}");
+            Console.WriteLine($"Day: {date.Day}");
+
+            Console.ReadLine();
+        }
+    }
+}
