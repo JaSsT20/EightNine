912291b7d2a4a4ebe60f9871faee9d394d5f45ae
Levid <jassontejada@gmail.com>
2023-02-11 11:31:42 -0400

    Adding struct Products

---
+++ Products.cs
@@ -0,0 +1,9 @@
+
+struct Products
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public string Description { get; set; }
+    public int QuantityAvailable { get; set; }
+}
